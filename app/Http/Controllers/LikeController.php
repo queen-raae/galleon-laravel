98a648523eb9576ee5556f4525ca7f1d6e3d4fe9
olavea <ola@olavea.com>
2025-10-29 08:34:32 +0100

    Created a LikeController and added the post functionality #13

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gateway;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Server side validation 
+        request()->validate([
+            'name' => '',
+            'like_button_toggled' => '',
+        ]); 
+        $liked = $request->boolean('like_button_toggled');
+        // $archived = $request->boolean('archived');
+        
+
+        Gateway::create([
+            'name' => request('name'),
+            'like_button_toggled' => $liked,
+        ]);
+
+        return redirect('/gateways');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gateway $gateway)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Gateway $gateway)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Gateway $gateway)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gateway $gateway)
+    {
+        //
+    }
+}

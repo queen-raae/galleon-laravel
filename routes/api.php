21767411ed55bf0b71820225103e06a74237da26
olavea <ola@olavea.com>
2025-11-04 09:33:10 +0100

    How do I see this route? The file is routes/ api.php #17

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['throttle:pub_like_api'])->group(function () 
 {
-    Route::get('/likes', function () {
-        return response()->json(Gateway::all(), 201);
+
+    //     How do I see this route? The file is routes/ api.php
+    Route::get('/likes', function () { // this
+        return response()->json(Gateway::all(), 201); // this
     }); 
 });
 

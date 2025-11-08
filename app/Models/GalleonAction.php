770b95033495050f342aa2967c3d947bd82f1653
olavea <ola@olavea.com>
2025-11-08 08:54:53 +0100

    Created model with related files #17

---
+++ app/Models/GalleonAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GalleonAction extends Model
+{
+    /** @use HasFactory<\Database\Factories\GalleonActionFactory> */
+    use HasFactory;
+}
